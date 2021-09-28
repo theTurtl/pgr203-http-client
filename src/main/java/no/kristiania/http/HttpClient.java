52c68c82a89a2717be1a1f2c0729327fcec29e8f
theTurtl <addimanni@gmail.com>
2021-09-28 19:48:24 +0200

    Make a HTTP connection to the server.

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -1,8 +1,27 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
 public class HttpClient {
 
-    public static void main(String[] args) {
-        System.out.println(("Hello world!"));
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("httpbin.org", 80);
+
+        socket.getOutputStream().write(
+                ("GET /html HTTP/1.1\r\n" +
+                        "Host: httpbin.org\r\n" +
+                        "\r\n").getBytes()
+        );
+
+        InputStream in = socket.getInputStream();
+
+        int c;
+        //-1 er ikke en gyldig byte. Derfor vil while loopen fortsette helt til
+        //det det forekommer bytes som ikke er gyldig.
+        while ((c = in.read()) != -1) {
+            System.out.print((char)c);
+        }
     }
 }
