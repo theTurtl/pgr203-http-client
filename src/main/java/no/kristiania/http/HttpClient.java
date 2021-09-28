b1ece31ef0533e0346abe171dce6d166bbc78d63
theTurtl <addimanni@gmail.com>
2021-09-28 23:29:35 +0200

    ShouldReturnStatusCode404(passed). modified methods in HttpClient class to be further dynamic.

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -3,33 +3,54 @@ package no.kristiania.http;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
+import java.sql.SQLOutput;
 
 public class HttpClient {
 
-    public HttpClient(String host, int port, String requestTarget) {
+    private final int statusCode;
+
+    public HttpClient(String host, int port, String requestTarget) throws IOException {
+        Socket socket = new Socket(host, port);
+
+
+        socket.getOutputStream().write(
+                ("GET " + requestTarget + " HTTP/1.1\r\n" +
+                        "Connection: close\r\n" +
+                        "Host: " + host + "\r\n" +
+                        "\r\n").getBytes()
+        );
+
+        //Med stringBuilder så kan en legge kontinuerlig legge en inn input.
+        //Her gjør vi det for ytelse, da det kontant vil komme inn ny input fra InputStream
+        //Her vil altså bytsene lest av while loopen komme. Igjen, i form av bytes (ASCII numre).
+        StringBuilder result = new StringBuilder();
+        InputStream in = socket.getInputStream();
+
+        int c;
+
+        //-1 er ikke en gyldig byte. Derfor vil while loopen fortsette helt til
+        //det det forekommer bytes som ikke er gyldig. bytes (0 - 255 some r gyldig)
+        while ((c = in.read()) != -1) {
+            result.append((char)c);
+        }
+
+        //Her legger vi inn alle ASCII numrene i en ny variabel (bytsene), konvertert til String verdier (istedet for Int).
+        String responseMessage = result.toString();
+
+        //Deretter lager vi et array bestemt av karakteren " " (mellomrom).
+        //Eksempel: før: 23452312 340433 9032; etter: {"23452312", "340433", "9032"}
+        //Så bestemmer vi hvilke av verdien vi vil hente fra arrayet, som her er [1] (altså andre verdi i arrayet).
+        //Dette passser overens, da statuskoden som hentes fra server kommer rett etter requestTarget (HTTP/1.1).
+        //Deretter plasserer vi denne verdien til en egen variabel, som vi kan senere returnere ved bruk (her: test klassen)
+        this.statusCode = Integer.parseInt(responseMessage.split(" ")[1]);
     }
 
     public static void main(String[] args) throws IOException {
         Socket socket = new Socket("httpbin.org", 80);
 
-        socket.getOutputStream().write(
-                ("GET /html HTTP/1.1\r\n" +
-                        "Connection: close\r\r" +
-                        "Host: httpbin.org\r\n" +
-                        "\r\n").getBytes()
-        );
-
-        InputStream in = socket.getInputStream();
-
-        int c;
-        //-1 er ikke en gyldig byte. Derfor vil while loopen fortsette helt til
-        //det det forekommer bytes som ikke er gyldig.
-        while ((c = in.read()) != -1) {
-            System.out.print((char)c);
-        }
     }
 
     public int getStatusCode() {
-        return 200;
+        return statusCode;
     }
 }
--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -2,6 +2,8 @@ package no.kristiania.http;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpClientTest {
@@ -12,14 +14,14 @@ class HttpClientTest {
     }
 
     @Test
-    void ShouldReturnStatusCode() {
+    void ShouldReturnStatusCode() throws IOException {
         HttpClient client = new HttpClient("httpbin.org", 80, "/html");
         assertEquals(200, client.getStatusCode());
     }
 
     @Test
-    void ShouldReturnStatusCode404() {
-        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+    void ShouldReturnStatusCode404() throws IOException {
+        HttpClient client = new HttpClient("httpbin.org", 80, "/htl");
         assertEquals(404, client.getStatusCode());
     }
 }
