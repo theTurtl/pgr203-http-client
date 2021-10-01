5af118d11f5e07969b855e762e2a3ac2950889ed
Clingycircle <ishu001@student.kristiania.no>
2021-10-01 19:28:11 +0200

    Working HTTP client! (#1)

--- src/test/java/no/kristiania/http/HttpClientTest.java
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -15,38 +15,32 @@ class HttpClientTest {
 
     @Test
     void ShouldReturnStatusCode() throws IOException {
-        HttpClient client = new HttpClient
-                ("httpbin.org", 80, "/html");
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
         assertEquals(200, client.getStatusCode());
     }
 
     @Test
     void ShouldReturnStatusCode404() throws IOException {
-        HttpClient client = new HttpClient
-                ("httpbin.org", 80, "/htl");
+        HttpClient client = new HttpClient("httpbin.org", 80, "/htl");
         assertEquals(404, client.getStatusCode());
     }
 
     @Test
     void ShouldReturnStatusHeaderFields() throws IOException {
-        HttpClient client = new HttpClient
-                ("httpbin.org", 80, "/html");
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
         assertEquals("text/html; charset=utf-8", client.getHeader("Content-Type"));
     }
 
     @Test
     void ShouldReadContentLenght() throws IOException {
-        HttpClient client = new HttpClient
-                ("httpbin.org", 80, "/html");
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
         assertEquals(3741, client.getContentLength());
     }
 
     @Test
     void shouldReadMessageBody() throws IOException {
-        HttpClient client = new HttpClient
-                ("httpbin.org", 80, "/html");
-        assertTrue(
-                client.getMessageBody().startsWith("\n<!DOCTYPE html>\n<htm"),
+        HttpClient client = new HttpClient("httpbin.org", 80, "/html");
+        assertTrue(client.getMessageBody().startsWith("\n<!DOCTYPE html>\n<htm"),
                 "should be html: " + client.getMessageBody()
 
         );
